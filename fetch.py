ab49a94b997a7fac5fcd99f2cca87a6852b92240
Jack Lockwood <jackjlockwood@gmail.com>
2025-07-03 00:32:01 -0400

    Reworked logic

--- fetch.py
+++ fetch.py
@@ -2,16 +2,15 @@ import requests as rq
 import gzip as gz
 import os
 
-URLS = ["https://www.nationstates.net/pages/nations.xml.gz","https://www.nationstates.net/pages/nations.xml.gz"]
+URLS = ["https://www.nationstates.net/pages/nations.xml.gz","https://www.nationstates.net/pages/regions.xml.gz"]
+DUMP_DIR = "dumps"
 
-def download_daily_dumps(urls:list[str],save_dir="dumps",filename=None):
-    try:
-        for url in urls:
-            os.makedirs(save_dir,exist_ok=True)
-            if filename is None:
-                filename = os.path.basename(url)
-                if not filename:
-                    filename = "newest_dump"
+def download_daily_dumps(urls:list[str]=URLS,save_dir=DUMP_DIR,filename=None):
+    print("Fetching daily dump files from NationStates...")
+    os.makedirs(save_dir,exist_ok=True)
+    for url in urls:
+        try:
+            filename = os.path.basename(url)
             save_path = os.path.join(save_dir,filename)
 
             response = rq.get(url,stream=True)
@@ -23,30 +22,27 @@ def download_daily_dumps(urls:list[str],save_dir="dumps",filename=None):
             
             print(f"File downloaded to {save_path}")
     
-    except rq.exceptions.RequestException as e:
-        print(f"Error downloading file: {e}")
-    except Exception as e:
-        print(f"Enexpected error: {e}")
+        except rq.exceptions.RequestException as e:
+            print(f"Error downloading file: {e}")
+        except Exception as e:
+            print(f"Enexpected error: {e}")
 
 def extract_data(files,extract_dirs:list[str]):
-    for file in files:
-        with gz.open(file,"rb") as f_in:
-            for extract_dir in extract_dirs:
-                extract_dir = extract_dirs.pop(0)
-                with open(extract_dir,"wb") as f_out:
+    print("Extracting XML data from dump files...")
+    for file, dest in zip(files,extract_dirs):
+        try:
+            with gz.open(file,"rb") as f_in:
+                with open(dest,"wb") as f_out:
                     f_out.write(f_in.read())
-                print(f"Extracted data to: {extract_dir}")
+        except FileNotFoundError:
+            print(f"Source file not found at {file}")
+        except Exception as e:
+            print(f"An error occurred while extracting {file}: {e}")
 
 
 def main():
-    regions_zip_file_path = r"\dumps\regions.xml.gz"
-    nations_zip_file_path = r"\dumps\nations.xml.gz"
-    extract_dir = r"dumps\regions.xml"
-    download_daily_dumps(urls=URLS)
+    regions_zip_file_path = r"dumps\regions.xml.gz"
+    nations_zip_file_path = r"dumps\nations.xml.gz"
+    download_daily_dumps()
 
     extract_data(files=[nations_zip_file_path,regions_zip_file_path],extract_dirs=[r"dumps\nations.xml",r"dumps\regions.xml"])
-
-if __name__ == "__main__":
-    main()
-else:
-    pass
